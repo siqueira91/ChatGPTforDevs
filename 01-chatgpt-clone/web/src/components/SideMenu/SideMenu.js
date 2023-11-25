0da3ace7df5f0199ef7680555e3758c9d39df9d4
isaias-siqueira <isaias.siqueiraa@gmail.com>
2023-11-25 03:00:03 -0300

    final project

---
+++ 01-chatgpt-clone/web/src/components/SideMenu/SideMenu.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './SideMenu.css'
+
+const SideMenu = ()=> {
+  return(
+    <aside className='sidemenu'>
+      <div className='sidemenu-button'>
+        <span>+</span>
+        Novo chat
+      </div>
+    </aside>
+  )
+}
+
+export default SideMenu
